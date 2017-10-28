c98515ca8e247a13bb96108773d51f54d53e4517
Christopher McIntyre <me@cgmcintyre.com>
2017-10-28 18:41:50 +0100

    Initial card setup

---
+++ src/components/HackLightCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hackcard">
+        <div>
+          <h3>{{ ghRepo.name }}</h3>
+          <h3>{{ ghRepo.owner.login }}</h3>
+          <h3 v-if="ghRepo.primaryLanguage" v-bind:style="{ 'background-color': ghRepo.primaryLanguage.color}">{{ ghRepo.primaryLanguage.name }}</h3>
+        </div>
+        <div>
+          {{ ghRepo.description }}
+        </div>
+        <ul>
+          <li>{{ ghRepo.stargazers.totalCount }}</li>
+          <li>{{ ghRepo.forks.totalCount }}</li>
+          <li>{{ ghRepo.issues.totalCount }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'hacklightlist',
+  props: ['ghRepo']
+}
+</script>
--- src/components/HackLightList.vue
+++ src/components/HackLightList.vue
@@ -1,34 +1,24 @@
 <template>
     <div class="wrapper">
-      This is where our component goes :^)
-      <div class="hackcard" v-for="repo in ghData.data">
-          <div>
-            <h3>{{ repo.name }}</h3>
-            <h3> - {{ repo.owner.login }}</h3>
-            <h3>{{ repo.primaryLanguage}}</h3>
-          </div>
-          <div>
-            {{ repo.description }}
-          </div>
-          <ul>
-            <li>{{ repo.stargazers.totalCount }}</li>
-            <li>{{ repo.forks.totalCount }}</li>
-            <li>{{ repo.issues.totalCount }}</li>
-          </ul>
-      </div>
+      <hacklightcard class="hackcard" :ghRepo="repo" v-for="repo in ghData.data">
+      </hacklightcard>
     </div>
 </template>
 
 <script>
+import HackLightCard from './HackLightCard.vue'
+
 export default {
   name: 'hacklightlist',
-  props: ['ghData']
+  props: ['ghData'],
+  components: {
+    hacklightcard: HackLightCard
+  }
 }
 </script>
 
 <style>
 .hackcard {
-    background-color: #69DCA4;
     margin: 5px 5px 5px 5px;
     padding: 5px 5px 5px 5px;
 }
