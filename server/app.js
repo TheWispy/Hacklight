a6b1c6c3e10ccb35dbafe88dc489ea0a0d2ab8cc
Daniel Carl Jones <danny@danielcarl.info>
2017-10-28 20:28:02 +0100

    Tidy up server files

--- server/app.js
+++ server/app.js
@@ -1,17 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var main = require('./routes/main');
-var api = require('./routes/api');
+// Routers
+const main = require('./routes/main');
+const api = require('./routes/api');
 
-var app = express();
+const app = express();
 
+// Middleware setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
+// Setup routers
 app.use('/', main);
 app.use('/api/', api);
 
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,10 +1,11 @@
-var path = require('path');
-var express = require('express');
-var router = express.Router();
+const path = require('path');
+const express = require('express');
+const router = express.Router();
 
 router.use('/dist', express.static(
     path.resolve(__dirname + '/../../dist')
 ));
+
 router.get('/', (req, res, next) => {
     res.sendFile('index.html', {
         root: './'
